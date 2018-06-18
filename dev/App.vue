fe7033a90cb2c032fb34c8e1daeb73a8cdd34694
天山来客 <hengwujun128@gmail.com>
2018-06-18 23:47:01 +0800

    modify publish file

--- dev/App.vue
+++ dev/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <h2>vue test vue-version</h2>
     <vue-version></vue-version>
   </div>
 </template>
  
 <script type="text/javascript">
-import VueVersion from "../src/index.js";
+// import VueVersion from "../src/index.js";
+import VueVersion from "../dist/vue-version.min";
 
 export default {
   components: {
